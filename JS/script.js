cfd2a31623721e04599ded277019bfd3bb5f4b36
Aleksei Arkhangelskii <arhmost@gmail.com>
2023-07-06 12:03:29 +0200

    third

--- JS/script.js
+++ JS/script.js
@@ -6,13 +6,28 @@ let personalMovieDB = {
     genres: [],
     privat: false,
 };
-let a = prompt('Один из последних просмотренных фильмов?'),
-    b = prompt('На сколько оцените его?'),
-    c = prompt('Один из последних просмотренных фильмов?'),
-    d = prompt('На сколько оцените его?');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let index = 0; index < 2; index++) {
+    let a = prompt('Один из последних просмотренных фильмов?'),
+        b = prompt('На сколько оцените его?');
+
+        if (a != null && b != null && a != '' & b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            index--;
+        }
+    }
+
+ if (personalMovieDB.count < 10) {
+    alert('Просмотренно довольно мало фильмов');
+ } else if (personalMovieDB.count > 30) {
+    alert('Вы киноман!')
+ } else {
+    alert('Вы классический зритель')
+ }         
+
 
 console.log(personalMovieDB);
 
